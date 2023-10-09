9514d765c1137940be338859d93dc57324f03d3f
mvazquez <matias.vazquezmartin@gmail.com>
2023-10-09 14:12:00 -0300

    feat: ejercicios extra

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -20,6 +20,23 @@ function numberOfCharacters(string) {
   // Las letras deben estar en orden alfabético.
   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
   // Tu código:
+
+  // const obj = {};
+  // for (const char of string) {
+  //   obj[char] = obj[char] ? obj[char] + 1 : 1;
+  // }
+  // return obj;
+
+  // return string.split('').reduce((obj, char) => {
+  //   obj[char] = obj[char] ? obj[char] + 1 : 1;
+  //   return obj;
+  // }, {});
+
+  const obj = {};
+  string.split("").forEach((char) => {
+    obj[char] = obj[char] ? obj[char] + 1 : 1;
+  });
+  return obj;
 }
 
 function capToFront(string) {
@@ -28,6 +45,29 @@ function capToFront(string) {
   // Retornar el string.
   // [EJEMPLO]: soyHENRY ---> HENRYsoy
   // Tu código:
+  // 1
+  // const sortedString = string.split('').sort((a, b) => a === a.toUpperCase() ? -1 : 1).join('');
+  // return sortedString;
+  // 2
+  // let uppercaseChars = '';
+  // let lowercaseChars = '';
+  // for (const char of string) {
+  //   if (char === char.toUpperCase()) {
+  //     uppercaseChars += char;
+  //   } else {
+  //     lowercaseChars += char;
+  //   }
+  // }
+  // return uppercaseChars + lowercaseChars;
+  const uppercaseChars = string
+    .split("")
+    .filter((char) => char === char.toUpperCase())
+    .join("");
+  const lowercaseChars = string
+    .split("")
+    .filter((char) => char === char.toLowerCase())
+    .join("");
+  return uppercaseChars + lowercaseChars;
 }
 
 function asAmirror(frase) {
@@ -35,18 +75,49 @@ function asAmirror(frase) {
   // La diferencia es que cada palabra estará escrita al inverso.
   // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
   // Tu código:
+  // 1
+  // return frase.split(' ').map(palabra => palabra.split('').reverse().join('')).join(' ');
+  const palabras = frase.split(" ");
+  const resultado = palabras
+    .map((palabra) => palabra.split("").reverse().join(""))
+    .join(" ");
+  return resultado;
 }
 
 function capicua(numero) {
   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
   // Caso contrario: "No es capicua".
   // Tu código:
+  const numeroComoString = numero.toString(); // Convertir el número a cadena
+  const numeroInvertido = numeroComoString.split("").reverse().join(""); // Invertir el número como cadena
+
+  if (numeroComoString === numeroInvertido) return "Es capicua";
+  return "No es capicua";
 }
 
 function deleteAbc(string) {
   // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
   // Retorna el string sin estas letras.
   // Tu código:
+  // 1
+  // const arr = string.split("")
+  // const str = arr.filter((char)=>{if(char !="a" && char !="b" && char!="c"){return char}})
+  // return str.join("")
+  // 2
+  // return string.replace(/[abc]/g, '');
+  // 3
+  // let result = '';
+  // for (const char of string) {
+  //   if (char !== 'a' && char !== 'b' && char !== 'c') {
+  //     result += char;
+  //   }
+  // }
+  // return result;
+
+  return string
+    .split("")
+    .filter((char) => char !== "a" && char !== "b" && char !== "c")
+    .join("");
 }
 
 function sortArray(arrayOfStrings) {
@@ -55,6 +126,9 @@ function sortArray(arrayOfStrings) {
   // de la longitud de cada string.
   // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
   // Tu código:
+  // 1
+  // return arrayOfStrings.map(str => str).sort((a, b) => a.length - b.length);
+  return arrayOfStrings.sort((a, b) => a.length - b.length);
 }
 
 function buscoInterseccion(array1, array2) {
@@ -64,6 +138,15 @@ function buscoInterseccion(array1, array2) {
   // Si no tienen elementos en común, retornar un arreglo vacío.
   // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
   // Tu código:
+  // 1
+  // const interseccion = array1.reduce((resultado, elemento) => {
+  //   if (array2.includes(elemento) && !resultado.includes(elemento)) {
+  //     resultado.push(elemento);
+  //   }
+  //   return resultado;
+  // }, []);
+  // return interseccion;
+  return array1.filter((elemento) => array2.includes(elemento));
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
